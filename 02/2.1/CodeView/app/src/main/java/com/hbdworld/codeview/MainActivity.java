60c72c41013640938588b3eff537d078f647abff
jinghewang <jinghewang@163.com>
2016-05-29 22:04:57 +0800

    CodeView click

--- 02/2.1/CodeView/app/src/main/java/com/hbdworld/codeview/MainActivity.java
+++ 02/2.1/CodeView/app/src/main/java/com/hbdworld/codeview/MainActivity.java
@@ -2,8 +2,13 @@ package com.hbdworld.codeview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        //textview
         TextView textView = new TextView(this);
+        textView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         textView.setText("我是文本");
         layout.addView(textView);
+
+        //button
+        Button button = new Button(this);
+        button.setText("buton");
+        button.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),view.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        layout.addView(button);
+
         setContentView(layout);
     }
 }
