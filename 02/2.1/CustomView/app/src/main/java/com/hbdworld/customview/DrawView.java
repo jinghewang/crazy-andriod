e5ab0faa08a9223290e24117bd0be1692c357cd2
jinghewang <jinghewang@163.com>
2016-05-30 00:20:42 +0800

    CustomView init

---
+++ 02/2.1/CustomView/app/src/main/java/com/hbdworld/customview/DrawView.java
@@ -0,0 +1,30 @@
+package com.hbdworld.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by hbd on 16/5/30.
+ */
+public class DrawView extends View {
+    public DrawView(Context context) {
+        super(context);
+    }
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
