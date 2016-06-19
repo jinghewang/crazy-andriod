145ed79a6c910565e33167a3674bb15d33e6075e
jinghewang <jinghewang@163.com>
2016-06-19 10:54:24 +0800

    MixView

--- 02/2.1/MixView/app/src/main/java/com/hbdworld/mixview/MainActivity.java
+++ 02/2.1/MixView/app/src/main/java/com/hbdworld/mixview/MainActivity.java
@@ -2,20 +2,43 @@ package com.hbdworld.mixview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    int[] images = new int[]{
+            R.drawable.ajax,
+            R.drawable.html,
+            R.drawable.java,
+            R.drawable.javaee,
+            R.drawable.swift
+    };
+    int currentImage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout main = (LinearLayout)this.findViewById(R.id.root);
+        LinearLayout main = (LinearLayout) this.findViewById(R.id.root);
         Button button = new Button(this);
-        button.setText("wewewewe");
+        button.setText(R.string.image_click);
         main.addView(button);
+
+        final ImageView imageView = new ImageView(this);
+        imageView.setImageResource(images[0]);
+        main.addView(imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(images[++currentImage % images.length]);
+            }
+        });
 
     }
 }
