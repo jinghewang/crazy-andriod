a6dfabf5fce0571a14e4a6c3475fd014aa366ac9
jinghewang <jinghewang@163.com>
2016-07-14 10:04:53 +0800

    RelativeLayoutTest ok

--- 02/2.2/RelativeLayoutTest/app/src/main/java/com/hbdworld/relativelayouttest/MainActivity.java
+++ 02/2.2/RelativeLayoutTest/app/src/main/java/com/hbdworld/relativelayouttest/MainActivity.java
@@ -2,6 +2,11 @@ package com.hbdworld.relativelayouttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemid = item.getItemId();
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_SHORT).show();
+
+        return super.onOptionsItemSelected(item);
     }
 }
