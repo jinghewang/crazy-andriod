d24159141478e2b62ab1d3a84c99773b4351e4f4
jinghewang <jinghewang@163.com>
2016-07-19 16:40:11 +0800

    togglebutton

--- 02/2.3/ButtonTest/app/src/main/java/com/hbdworld/buttontest/MainActivity.java
+++ 02/2.3/ButtonTest/app/src/main/java/com/hbdworld/buttontest/MainActivity.java
@@ -8,20 +8,48 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView show = null;
     Button start = null;
     Button stop = null;
+    ToggleButton tb ;
+    Switch sw ;
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        tb = (ToggleButton)this.findViewById(R.id.tb);
+        sw = (Switch)this.findViewById(R.id.sw);
+        layout = (LinearLayout)this.findViewById(R.id.layout);
+
+        CompoundButton.OnCheckedChangeListener listener  = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                tb.setChecked(b);
+                sw.setChecked(b);
+                if (b){
+                    layout.setOrientation(LinearLayout.HORIZONTAL);
+                }
+                else{
+                    layout.setOrientation(LinearLayout.VERTICAL);
+                }
+            }
+        };
+
+        tb.setOnCheckedChangeListener(listener);
+        sw.setOnCheckedChangeListener(listener);
 
     }
 
