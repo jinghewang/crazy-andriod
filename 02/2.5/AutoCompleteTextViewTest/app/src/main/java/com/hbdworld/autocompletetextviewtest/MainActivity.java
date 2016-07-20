e002c7cfb5e8c304a3fe6d7c971c012d9a0b60b3
jinghewang <jinghewang@163.com>
2016-07-21 00:38:12 +0800

    AutoCompleteTextViewTest ok

--- 02/2.5/AutoCompleteTextViewTest/app/src/main/java/com/hbdworld/autocompletetextviewtest/MainActivity.java
+++ 02/2.5/AutoCompleteTextViewTest/app/src/main/java/com/hbdworld/autocompletetextviewtest/MainActivity.java
@@ -2,12 +2,33 @@ package com.hbdworld.autocompletetextviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListAdapter;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView auto;
+    MultiAutoCompleteTextView mauto;
+    String[] books = new String[]{
+            "疯狂Java讲义",
+            "疯狂Ajax讲义",
+            "疯狂XML讲义",
+            "疯狂Workflow讲义"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //--
+        auto = (AutoCompleteTextView)this.findViewById(R.id.auto);
+        mauto = (MultiAutoCompleteTextView)this.findViewById(R.id.mauto);
+
+        ArrayAdapter listAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,books);
+        auto.setAdapter(listAdapter);
+        mauto.setAdapter(listAdapter);
+        mauto.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
     }
 }
