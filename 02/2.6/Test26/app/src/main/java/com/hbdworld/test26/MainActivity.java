dfcd01eb8bd19496799ea83c006fab4ae72c539e
jinghewang <jinghewang@163.com>
2016-07-24 01:02:55 +0800

    RatingBar

--- 02/2.6/Test26/app/src/main/java/com/hbdworld/test26/MainActivity.java
+++ 02/2.6/Test26/app/src/main/java/com/hbdworld/test26/MainActivity.java
@@ -4,57 +4,30 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 该程序模拟填充长度为100的数组
-    private int[] data = new int[100];
-    int hasData = 0;
-    // 记录ProgressBar的完成进度
-    int status = 0;
-    ProgressBar bar , bar2;
-
-    Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            if (msg.what == 0x111)
-                bar.setProgress(status);
-            //super.handleMessage(msg);
-        }
-    };
+    RatingBar ratingBar ;
+    ImageView imageView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
-        bar = (ProgressBar)this.findViewById(R.id.progressBar);
-        new Thread(){
+
+        //--
+        ratingBar = (RatingBar)this.findViewById(R.id.ratingBar);
+        imageView = (ImageView)this.findViewById(R.id.imageView);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
-
-            public void run() {
-                //super.run();
-
-                while (status < 100){
-                    status = doWork();
-                    handler.sendEmptyMessage(0x111);
-                }
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(MainActivity.this,String.valueOf(rating),Toast.LENGTH_SHORT).show();
             }
-        }.start();
-    }
-
-    public int doWork(){
-        // 为数组元素赋值
-        data[hasData++] = (int) (Math.random() * 100);
-        try
-        {
-            Thread.sleep(100);
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
-        return hasData;
+        });
     }
 }
