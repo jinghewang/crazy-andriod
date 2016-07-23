3b6781357858700b75511a37fdf3ea94efbac1eb
jinghewang <jinghewang@163.com>
2016-07-24 01:28:38 +0800

    TitleProgressBar

--- 02/2.6/Test26/app/src/main/java/com/hbdworld/test26/MainActivity.java
+++ 02/2.6/Test26/app/src/main/java/com/hbdworld/test26/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
@@ -12,33 +15,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    SeekBar seekBar;
-
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_PROGRESS);  //①
         setContentView(R.layout.activity_main);
 
         //--
-        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
-        imageView = (ImageView)this.findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.lijiang);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        Button bn1 = (Button)findViewById(R.id.bn1);
+        Button bn2 = (Button)findViewById(R.id.bn2);
+        bn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                imageView.setAlpha(i);
+            public void onClick(View view) {
+                setProgressBarIndeterminate(true);
+                setProgressBarVisibility(true);
+                setProgress(4500);
             }
+        });
 
+        bn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
+            public void onClick(View view) {
+                setProgressBarIndeterminate(false);
+                setProgressBarVisibility(false);
+                //setProgress(4500);
             }
         });
     }
