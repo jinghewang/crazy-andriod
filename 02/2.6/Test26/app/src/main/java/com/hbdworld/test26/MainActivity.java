b7d1328b8cdaeaf2338df70133b561e329d24804
jinghewang <jinghewang@163.com>
2016-07-24 23:26:40 +0800

    imageSwitcher Test

--- 02/2.6/Test26/app/src/main/java/com/hbdworld/test26/MainActivity.java
+++ 02/2.6/Test26/app/src/main/java/com/hbdworld/test26/MainActivity.java
@@ -5,41 +5,83 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] imageIds = new int[]
+            {
+                    R.drawable.bomb5, R.drawable.bomb6, R.drawable.bomb7
+                    , R.drawable.bomb8, R.drawable.bomb9, R.drawable.bomb10
+                    , R.drawable.bomb11, R.drawable.bomb12, R.drawable.bomb13
+                    , R.drawable.bomb14, R.drawable.bomb15, R.drawable.bomb16
+            };
+
+    ImageSwitcher imageSwitcher;
+    GridView gridView;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_PROGRESS);  //①
         setContentView(R.layout.activity_main);
 
         //--
-        Button bn1 = (Button)findViewById(R.id.bn1);
-        Button bn2 = (Button)findViewById(R.id.bn2);
-        bn1.setOnClickListener(new View.OnClickListener() {
+        imageSwitcher = (ImageSwitcher)this.findViewById(R.id.imageSwitcher);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
             @Override
-            public void onClick(View view) {
-                setProgressBarIndeterminate(true);
-                setProgressBarVisibility(true);
-                setProgress(4500);
+            public View makeView() {
+                ImageView image = new ImageView(MainActivity.this);
+                image.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                image.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                return image;
             }
         });
 
-        bn2.setOnClickListener(new View.OnClickListener() {
+        gridView = (GridView)this.findViewById(R.id.gridView);
+        List<Map<String,Object>> items = new ArrayList<>();
+        for (int i = 0; i < imageIds.length; i++) {
+            Map<String,Object> item = new HashMap<>();
+            item.put("image",imageIds[i]);
+            items.add(item);
+        }
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,items,R.layout.cell,new String[]{"image"},new int[]{R.id.imageView});
+        gridView.setAdapter(simpleAdapter);
+
+        gridView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                setProgressBarIndeterminate(false);
-                setProgressBarVisibility(false);
-                //setProgress(4500);
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                imageSwitcher.setImageResource(imageIds[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                imageSwitcher.setImageResource(imageIds[i]);
             }
         });
     }
