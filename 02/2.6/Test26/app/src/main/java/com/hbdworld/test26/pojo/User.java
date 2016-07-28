6fcb55c41e070e5ff16337ba2a637b50a4fc3a0f
jinghewang <jinghewang@163.com>
2016-07-28 09:37:14 +0800

    AlertDialog ok 2

---
+++ 02/2.6/Test26/app/src/main/java/com/hbdworld/test26/pojo/User.java
@@ -0,0 +1,26 @@
+package com.hbdworld.test26.pojo;
+
+/**
+ * Created by hbd on 16/7/28.
+ */
+public class User {
+
+    private String uid;
+    private String pwd;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
---
+++ 02/2.6/Test26/app/src/main/java/com/hbdworld/test26/utils/CrazyUtils.java
@@ -0,0 +1,10 @@
+package com.hbdworld.test26.utils;
+
+/**
+ * Created by hbd on 16/7/28.
+ */
+public class CrazyUtils {
+
+
+
+}
