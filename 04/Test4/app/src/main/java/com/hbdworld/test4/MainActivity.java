78a8838869c63ddede3331cb867ebd07f5c0f285
jinghewang <jinghewang@163.com>
2016-08-16 09:56:58 +0800

    Test4  init  33

--- 04/Test4/app/src/main/java/com/hbdworld/test4/MainActivity.java
+++ 04/Test4/app/src/main/java/com/hbdworld/test4/MainActivity.java
@@ -3,11 +3,17 @@ package com.hbdworld.test4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BookListFragment.Callbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_twopane);
+    }
+
+    @Override
+    public void onItemSelected(Integer id) {
+        // 创建Bundle，准备向Fragment传入参数
+        Bundle arguments = new Bundle();
     }
 }
