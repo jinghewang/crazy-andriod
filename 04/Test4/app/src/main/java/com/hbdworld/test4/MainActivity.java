d4abe359f8726cd7da4420c476f2f355b82fd438
jinghewang <jinghewang@163.com>
2016-08-21 21:39:18 +0800

    actionattr

--- 04/Test4/app/src/main/java/com/hbdworld/test4/MainActivity.java
+++ 04/Test4/app/src/main/java/com/hbdworld/test4/MainActivity.java
@@ -8,68 +8,32 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button startActivity, addFragment, backFragment, replaceFragment, finish;
+
+    private static final String ACTION_SECOND = "com.hbdworld.test4.intent.action.SECOND_ACTION";
+    private static final String ACTION_HELLO = "helloWorld";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //--
-        startActivity = (Button) findViewById(R.id.startActivity);
-        addFragment = (Button) findViewById(R.id.addFragment);
-        backFragment = (Button) findViewById(R.id.backFragment);
-        replaceFragment = (Button) findViewById(R.id.replaceFragment);
-        finish = (Button) findViewById(R.id.finish);
 
-        //启动对话框风格的Activity
-        startActivity.setOnClickListener(new View.OnClickListener() {
+        Button start = (Button)this.findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View source) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.ACTION_SECOND);
                 startActivity(intent);
             }
         });
-        // 加载目标Fragment
-        addFragment.setOnClickListener(new View.OnClickListener() {
+
+        Button start2 = (Button)this.findViewById(R.id.start2);
+        start2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View source) {
-
-                LifecycleFragment fragment = new LifecycleFragment();
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.container,fragment)
-                        .commit();
-
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.ACTION_HELLO);
+                startActivity(intent);
             }
         });
-        // 替换目标Fragment、并加入Back栈
-        backFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View source) {
-                SecondFragment fragment = new SecondFragment();
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.container,fragment)
-                        .addToBackStack("aa")
-                        .commit();
-            }
-        });
-        // 为replaceFragment按钮绑定事件监听器
-        replaceFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SecondFragment fragment = new SecondFragment();
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.container, fragment)
-                        .commit();
-            }
-        });
-        // 为finish按钮绑定事件监听器
-        finish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View source) {
-                MainActivity.this.finish();;
-            }
-        });
-
-
     }
 }
--- 04/Test4/app/src/main/java/com/hbdworld/test4/SecondActivity.java
+++ 04/Test4/app/src/main/java/com/hbdworld/test4/SecondActivity.java
@@ -10,8 +10,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //--
         TextView textView = new TextView(this);
-        textView.setText(this.getLocalClassName());
+        textView.setText(this.getIntent().getAction());
         this.setContentView(textView);
     }
 }
