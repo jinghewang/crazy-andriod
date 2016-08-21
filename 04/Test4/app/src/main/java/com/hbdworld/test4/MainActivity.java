bf75c8471cce4d09224703b890876481d13c2e46
jinghewang <jinghewang@163.com>
2016-08-21 23:28:06 +0800

    bindOnClickListener

--- 04/Test4/app/src/main/java/com/hbdworld/test4/MainActivity.java
+++ 04/Test4/app/src/main/java/com/hbdworld/test4/MainActivity.java
@@ -1,12 +1,14 @@
 package com.hbdworld.test4;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private static final String ACTION_SECOND = "com.hbdworld.test4.intent.action.SECOND_ACTION";
@@ -18,33 +20,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //--
+        this.bindOnClickListener(this, this, new int[]{R.id.start, R.id.start2, R.id.start3, R.id.bn, R.id.call, R.id.edit});
+    }
 
-        Button start = (Button)this.findViewById(R.id.start);
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.ACTION_SECOND);
+    public Button findButtonById(int view) {
+        return (Button) this.findViewById(view);
+    }
+
+
+    public void bindOnClickListener(Activity activity, View.OnClickListener listener, int[] views) {
+        for (int view : views) {
+            View v = activity.findViewById(view);
+            v.setOnClickListener(listener);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.start:
+                intent = new Intent(MainActivity.ACTION_SECOND);
                 startActivity(intent);
-            }
-        });
+                break;
 
-        Button start2 = (Button)this.findViewById(R.id.start2);
-        start2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.ACTION_HELLO);
+            case R.id.start2:
+                intent = new Intent(MainActivity.ACTION_HELLO);
                 startActivity(intent);
-            }
-        });
+                break;
 
-        Button start3 = (Button)this.findViewById(R.id.start3);
-        start3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.ACTION_SECOND);
+            case R.id.start3:
+                intent = new Intent(MainActivity.ACTION_SECOND);
                 intent.addCategory(CATEGORY_SECOND);
                 startActivity(intent);
-            }
-        });
+                break;
+
+            case R.id.bn:
+
+                break;
+
+            case R.id.call:
+
+                break;
+
+            case R.id.edit:
+
+                break;
+
+            default:
+                break;
+
+        }
     }
 }
