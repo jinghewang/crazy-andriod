c5ee195af04605e6ad78dc19fa6c2ef3e1cb60fe
jinghewang <jinghewang@163.com>
2016-09-04 00:20:38 +0800

    Test10 TelephoneStatus

--- 10/Test10/app/src/main/java/com/hbdworld/test10/MainActivity.java
+++ 10/Test10/app/src/main/java/com/hbdworld/test10/MainActivity.java
@@ -11,6 +11,8 @@ import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,72 +30,89 @@ import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     ArrayList<String> blockList = new ArrayList<>();
-
-
-    // 判断某个电话号码是否在黑名单之内
-    public boolean isBlock(String phone) {
-        for (String s1 : blockList) {
-            if (s1.equals(phone)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    ListView showView;
+    // 声明代表状态名的数组
+    String[] statusNames;
+    TelephonyManager tManager;
+    ArrayList<String> statusValues = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        this.showView = (ListView) this.findViewById(R.id.show);
         this.bindOnClickListener(this, R.id.read, R.id.write);
+
+        tManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        statusNames = this.getResources().getStringArray(R.array.statusNames);
+        // 获取代表SIM卡状态的数组
+        String[] simState = getResources().getStringArray(R.array.simState);
+        // 获取代表电话网络类型的数组
+        String[] phoneType = getResources().getStringArray(R.array.phoneType);
+
+        // 获取设备编号
+        statusValues.add(tManager.getDeviceId());
+        // 获取系统平台的版本
+        statusValues.add(tManager.getDeviceSoftwareVersion()
+                != null ? tManager.getDeviceSoftwareVersion() : "未知");
+        // 获取网络运营商代号
+        statusValues.add(tManager.getNetworkOperator());
+        // 获取网络运营商名称
+        statusValues.add(tManager.getNetworkOperatorName());
+        // 获取手机网络类型
+        statusValues.add(phoneType[tManager.getPhoneType()]);
+        // 获取设备所在位置
+        statusValues.add(tManager.getCellLocation() != null ? tManager
+                .getCellLocation().toString() : "未知位置");
+        // 获取SIM卡的国别
+        statusValues.add(tManager.getSimCountryIso());
+        // 获取SIM卡序列号
+        statusValues.add(tManager.getSimSerialNumber());
+        // 获取SIM卡状态
+        statusValues.add(simState[tManager.getSimState()]);
+
+        ArrayList<Map<String, String>> status = new ArrayList<>();
+        // 遍历statusValues集合，将statusNames、statusValues
+        // 的数据封装到List<Map<String , String>>集合中
+        for (int i = 0; i < statusValues.size(); i++)
+        {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("name", statusNames[i]);
+            map.put("value", statusValues.get(i));
+            status.add(map);
+        }
+        // 使用SimpleAdapter封装List数据
+        SimpleAdapter adapter = new SimpleAdapter(this, status,
+                R.layout.line, new String[] { "name", "value" }
+                , new int[] { R.id.name, R.id.value });
+        // 为ListView设置Adapter
+        showView.setAdapter(adapter);
+
+
     }
+
 
     @Override
     public void onClick(View view) {
         //showToast("----onClick:" + view.getId());
         Button btn = (Button) view;
-        TextView textView = (TextView)this.findViewById(R.id.msg);
+        TextView textView = (TextView) this.findViewById(R.id.msg);
         String fileName = getFilesDir() + "hbd.txt";
         Intent intent = null;
         String content = "";
         switch (btn.getId()) {
             case R.id.read:
-                try {
-                    InputStream instream = new FileInputStream("hbd.txt");
-                    if (instream != null)
-                    {
-                        InputStreamReader inputreader = new InputStreamReader(instream);
-                        BufferedReader buffreader = new BufferedReader(inputreader);
-                        String line;
-                        //分行读取
-                        while (( line = buffreader.readLine()) != null) {
-                            content += line + "\n";
-                        }
-                        instream.close();
-                    }
-                    textView.setText(content);
-                    showToast("读取成功");
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
                 break;
 
             case R.id.write:
-                try {
-                    FileOutputStream fos = openFileOutput("hbd.txt", MODE_APPEND);
-                    PrintStream ps = new PrintStream(fos);
-                    ps.println(String.valueOf(System.currentTimeMillis()));
-                    ps.close();
-                    showToast("写入成功");
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
                 break;
 
             default:
