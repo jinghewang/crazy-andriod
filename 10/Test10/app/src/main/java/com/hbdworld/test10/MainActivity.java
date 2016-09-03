a111d4cc88f61e6fa775791b2baac31fac8d5cdf
jinghewang <jinghewang@163.com>
2016-09-03 15:07:58 +0800

    Test10 init

---
+++ 10/Test10/app/src/main/java/com/hbdworld/test10/MainActivity.java
@@ -0,0 +1,63 @@
+package com.hbdworld.test10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        showToast("----onClick:" + view.getId());
+        Button btn = (Button) view;
+        switch (btn.getId()) {
+
+            default:
+                break;
+        }
+    }
+
+
+    public void showToast(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public Button findButtonById(int view) {
+        return (Button) this.findViewById(view);
+    }
+
+    public TextView findTextViewById(int view) {
+        return (TextView) this.findViewById(view);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends View> T getView(int id) {
+        try {
+            return (T) this.findViewById(id);
+        } catch (ClassCastException ex) {
+            throw ex;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends View> T getView2(Class<T> view, int id) {
+        return (T) this.findViewById(id);
+    }
+
+    public void bindOnClickListener(View.OnClickListener listener, int... views) {
+        for (int view : views) {
+            View v = this.findViewById(view);
+            v.setOnClickListener(listener);
+        }
+    }
+}
