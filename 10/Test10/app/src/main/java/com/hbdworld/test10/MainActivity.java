5f08f25382d891fafd8ada70f6eda634c7f40b51
jinghewang <jinghewang@163.com>
2016-09-04 18:25:48 +0800

    Test10 audoplayer

--- 10/Test10/app/src/main/java/com/hbdworld/test10/MainActivity.java
+++ 10/Test10/app/src/main/java/com/hbdworld/test10/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Service;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.TimedText;
+import android.net.rtp.AudioStream;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,10 +15,12 @@ import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -36,67 +42,37 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    ArrayList<String> blockList = new ArrayList<>();
-    ListView showView;
-    // 声明代表状态名的数组
-    String[] statusNames;
-    TelephonyManager tManager;
-    ArrayList<String> statusValues = new ArrayList<>();
+    AudioManager audioManager;
+    MediaPlayer mediaPlayer ;
+    boolean isMute = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        this.showView = (ListView) this.findViewById(R.id.show);
-        this.bindOnClickListener(this, R.id.read, R.id.write);
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.earth);
 
-        tManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-        statusNames = this.getResources().getStringArray(R.array.statusNames);
-        // 获取代表SIM卡状态的数组
-        String[] simState = getResources().getStringArray(R.array.simState);
-        // 获取代表电话网络类型的数组
-        String[] phoneType = getResources().getStringArray(R.array.phoneType);
-
-        // 获取设备编号
-        statusValues.add(tManager.getDeviceId());
-        // 获取系统平台的版本
-        statusValues.add(tManager.getDeviceSoftwareVersion()
-                != null ? tManager.getDeviceSoftwareVersion() : "未知");
-        // 获取网络运营商代号
-        statusValues.add(tManager.getNetworkOperator());
-        // 获取网络运营商名称
-        statusValues.add(tManager.getNetworkOperatorName());
-        // 获取手机网络类型
-        statusValues.add(phoneType[tManager.getPhoneType()]);
-        // 获取设备所在位置
-        statusValues.add(tManager.getCellLocation() != null ? tManager
-                .getCellLocation().toString() : "未知位置");
-        // 获取SIM卡的国别
-        statusValues.add(tManager.getSimCountryIso());
-        // 获取SIM卡序列号
-        statusValues.add(tManager.getSimSerialNumber());
-        // 获取SIM卡状态
-        statusValues.add(simState[tManager.getSimState()]);
-
-        ArrayList<Map<String, String>> status = new ArrayList<>();
-        // 遍历statusValues集合，将statusNames、statusValues
-        // 的数据封装到List<Map<String , String>>集合中
-        for (int i = 0; i < statusValues.size(); i++)
-        {
-            HashMap<String, String> map = new HashMap<>();
-            map.put("name", statusNames[i]);
-            map.put("value", statusValues.get(i));
-            status.add(map);
-        }
-        // 使用SimpleAdapter封装List数据
-        SimpleAdapter adapter = new SimpleAdapter(this, status,
-                R.layout.line, new String[] { "name", "value" }
-                , new int[] { R.id.name, R.id.value });
-        // 为ListView设置Adapter
-        showView.setAdapter(adapter);
+        mediaPlayer.setOnTimedTextListener(new MediaPlayer.OnTimedTextListener() {
+            @Override
+            public void onTimedText(MediaPlayer mediaPlayer, TimedText timedText) {
+                showToast(""+ timedText);
+            }
+        });
 
 
+
+        ToggleButton toggleButton = (ToggleButton)this.findViewById(R.id.mute);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                audioManager.setStreamMute(AudioManager.STREAM_MUSIC,checked);
+            }
+        });
+
+        this.bindOnClickListener(this,R.id.play,R.id.stop,R.id.up,R.id.down,R.id.mute);
     }
 
 
@@ -104,15 +80,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         //showToast("----onClick:" + view.getId());
         Button btn = (Button) view;
-        TextView textView = (TextView) this.findViewById(R.id.msg);
-        String fileName = getFilesDir() + "hbd.txt";
-        Intent intent = null;
-        String content = "";
         switch (btn.getId()) {
-            case R.id.read:
+            case R.id.play:
+                mediaPlayer.start();
                 break;
 
-            case R.id.write:
+            case R.id.stop:
+                mediaPlayer.stop();
+                break;
+
+            case R.id.up:
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_RAISE,AudioManager.FLAG_SHOW_UI);
+                break;
+
+            case R.id.down:
+                audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_LOWER,AudioManager.FLAG_SHOW_UI);
                 break;
 
             default:
