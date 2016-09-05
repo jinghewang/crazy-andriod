df3ce908c8ecd85795afa12bd51dc31e9baac5c7
jinghewang <jinghewang@163.com>
2016-09-05 09:10:27 +0800

    Test10 register receiver

--- 10/Test10/app/src/main/java/com/hbdworld/test10/MainActivity.java
+++ 10/Test10/app/src/main/java/com/hbdworld/test10/MainActivity.java
@@ -1,62 +1,60 @@
 package com.hbdworld.test10;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.app.Service;
-import android.app.TimePickerDialog;
-import android.content.ComponentName;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.media.TimedText;
-import android.net.rtp.AudioStream;
-import android.os.IBinder;
-import android.os.Vibrator;
-import android.support.v7.app.AlertDialog;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
-import android.widget.ToggleButton;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // 获取界面中显示歌曲标题、作者文本框
+    TextView title, author;
+    // 播放/暂停、停止按钮
+    ImageButton play, stop;
+    ActivityReceiver activityReceiver;
+    public static final String CTL_ACTION = "org.crazyit.action.CTL_ACTION";
+    public static final String UPDATE_ACTION = "org.crazyit.action.UPDATE_ACTION";
+    // 定义音乐的播放状态，0x11代表没有播放；0x12代表正在播放；0x13代表暂停
+    int status = 0x11;
+    String[] titleStrs = new String[]{"心愿", "约定", "美丽新世界"};
+    String[] authorStrs = new String[]{"未知艺术家", "周蕙", "伍佰"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        title = (TextView) findViewById(R.id.title);
+        author = (TextView) findViewById(R.id.author);
+
+        //receiver
+        activityReceiver = new ActivityReceiver();
+        IntentFilter filter =  new IntentFilter();
+        filter.addAction(UPDATE_ACTION);
+        registerReceiver(activityReceiver,filter);
+
+        //service
+        Intent intent = new Intent(MainActivity.this,MusicService.class);
+        startService(intent);
+
         this.bindOnClickListener(this, R.id.play,R.id.stop);
     }
 
+
+    public class ActivityReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    }
 
     @Override
     public void onClick(View view) {
@@ -64,18 +62,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent = null;
         switch (btn.getId()) {
             case R.id.play:
-                intent = new Intent(MainActivity.this, MyReceiver.class);
-                intent.putExtra("msg","hello world");
-                this.sendBroadcast(intent);
-                showToast("发送成功");
                 break;
 
             case R.id.stop:
-                intent = new Intent();
-                intent.setAction("com.hbdworld.action.ACTION_RECEIVER");
-                intent.putExtra("msg","hello world2");
-                this.sendOrderedBroadcast(intent,null);
-                showToast("发送成功2");
                 break;
 
             default:
--- 10/Test10/app/src/main/java/com/hbdworld/test10/MusicService.java
+++ 10/Test10/app/src/main/java/com/hbdworld/test10/MusicService.java
@@ -1,10 +1,24 @@
 package com.hbdworld.test10;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
 public class MusicService extends Service {
+
+    MyReceiver serviceReceiver;
+    AssetManager am;
+    String[] musics = new String[]{"wish.mp3", "promise.mp3", "beautiful.mp3"};
+    MediaPlayer mPlayer;
+    // 当前的状态，0x11代表没有播放；0x12代表正在播放；0x13代表暂停
+    int status = 0x11;
+    // 记录当前正在播放的音乐
+    int current = 0;
+
     public MusicService() {
     }
 
@@ -12,5 +26,13 @@ public class MusicService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public class MyReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
     }
 }
