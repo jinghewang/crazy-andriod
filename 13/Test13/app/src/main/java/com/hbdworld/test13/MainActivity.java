9d51b863cd301095fc469123ef93d0e16e145698
jinghewang <jinghewang@163.com>
2016-09-11 21:53:55 +0800

    Test13 init 2

--- 13/Test13/app/src/main/java/com/hbdworld/test13/MainActivity.java
+++ 13/Test13/app/src/main/java/com/hbdworld/test13/MainActivity.java
@@ -2,12 +2,74 @@ package com.hbdworld.test13;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        //--
+
+
+
+        //--
+        bindOnClickListener(this, R.id.play, R.id.stop);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Button btn = (Button) view;
+        switch (btn.getId()) {
+            case R.id.play:
+
+                break;
+
+            case R.id.stop:
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    public void showToast(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public Button findButtonById(int view) {
+        return (Button) this.findViewById(view);
+    }
+
+    public TextView findTextViewById(int view) {
+        return (TextView) this.findViewById(view);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends View> T getView(int id) {
+        try {
+            return (T) this.findViewById(id);
+        } catch (ClassCastException ex) {
+            throw ex;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends View> T getView2(Class<T> view, int id) {
+        return (T) this.findViewById(id);
+    }
+
+    public void bindOnClickListener(View.OnClickListener listener, int... views) {
+        for (int view : views) {
+            View v = this.findViewById(view);
+            v.setOnClickListener(listener);
+        }
     }
 }
